15d00cb23b55e196b19f5f076bc51e1bdc92497f
huishanchiu <jolley0901@gmail.com>
2021-10-20 09:35:50 +0800

    Add animation when scrolling the landing page

---
+++ chill/src/App.js
@@ -0,0 +1,11 @@
+import Landing from "./LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ chill/src/LandingPage/theme.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "../theme.css";
+
+function Theme() {
+  return (
+    <div>
+      <div class="theme">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere
+        distinctio numquam debitis sapiente adipisci delectus assumenda tempore
+        aperiam molestiae ex. Officia est omnis ab facere tempora atque
+        molestias doloribus cupiditate.
+      </div>
+    </div>
+  );
+}
+export default Theme;
